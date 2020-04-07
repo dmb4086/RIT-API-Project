b26de8d619bf0c8b5ad89d7819486969b8dc6537
klw2488 <36055783+klw2488@users.noreply.github.com>
2020-04-07 12:05:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,16 @@ function buildOverview() {
     let url = "www.ist.rit.edu/api/about";
     let data = "";
     let success = ""
-    
+
+    $.getJSON(url, function(data) {
+
+        var text = `Date: ${data.date}<br>
+                    Time: ${data.time}<br>
+                    Unix time: ${data.milliseconds_since_epoch}`
+
+
+        console.log(text);
+        console.log("test");
+    });
 
 }
