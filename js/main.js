a3d8b4ca7ddef63405cbbe21bda357a58c54bebc
klw2488 <36055783+klw2488@users.noreply.github.com>
2020-04-07 12:30:37 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
 
 function buildOverview() {
-    let url = "www.ist.rit.edu/api/about";
-    let data = "";
-    let success = ""
+    let url = "http://www.ist.rit.edu/api/about/";
 
-    $.getJSON(url, function(data) {
-
-        var text = `Date: ${data.date}<br>
-                    Time: ${data.time}<br>
-                    Unix time: ${data.milliseconds_since_epoch}`
+    var response = getText(url);
+    console.log(response);
 
 
-        console.log(text);
-        console.log("test");
-    });
 
+
+
+}
+
+function getText(url) {
+    var request = new XMLHttpRequest();
+
+    request.open("GET", url);
+
+    request.setRequestHeader("Content-type", "application/json");
+
+    request.onreadystatechange = function() { // Define event listener
+        // If the request is compete and was successful
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader("Content-Type");
+            console.log(type);
+            if (type.match("application/json")) // Make sure response is json
+                callback(request.response);
+
+            if (type.indexOf("xml") !== -1 && request.responseXML)
+                callback(request.responseXML); // Document response
+            else if (type === "application/json")
+                callback(JSON.parse(request.responseText)); // JSON response
+            else
+                callback(request.responseText); // String response
+        }
+    };
+    request.send(null);
 }
